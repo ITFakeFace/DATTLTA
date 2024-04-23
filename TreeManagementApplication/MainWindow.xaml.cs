995f13d8a82248d7e37d9cf655c7dcf151c8ec24
ITFakeFace <dthung6604@gmail.com>
2024-04-23 16:50:07 +0700

    Alternative Old Commit

--- TreeManagementApplication/MainWindow.xaml.cs
+++ TreeManagementApplication/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace TreeManagementApplication
 		CoordinateCalculator coordinateCalculator;
 		ITree<int> Tree = new BinarySearchTree<int>();
 		int GridSize;
-
+		int a;
 		public MainWindow()
 		{
 			InitializeComponent();
