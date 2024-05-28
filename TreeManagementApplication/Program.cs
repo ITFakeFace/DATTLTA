6848f72d9a9bd7f22e56b0aba465d1f208f4101f
KhoiNguyen <khoinm0603@gmail.com>
2024-05-28 12:06:03 +0700

    Clean Program.cs

--- TreeManagementApplication/Program.cs
+++ TreeManagementApplication/Program.cs
@@ -13,29 +13,9 @@ namespace TreeManagementApplication
         public static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            Console.WriteLine("Console is running");
-            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>();
             var app = new App();
             app.InitializeComponent();
             app.Run();
-
-
-            Console.WriteLine("");
-
-
-            binarySearchTree.InsertNode(50);
-            binarySearchTree.InsertNode(200);
-            binarySearchTree.InsertNode(25);
-            binarySearchTree.InsertNode(75);
-            binarySearchTree.InsertNode(150);
-            binarySearchTree.InsertNode(300);
-            binarySearchTree.InsertNode(37);
-            binarySearchTree.InsertNode(67);
-            binarySearchTree.InsertNode(87);
-            binarySearchTree.GenerateGridIndex();
-            binarySearchTree.PrintConsole();
-            /*textFile.fileWriter(binarySearchTree.Serialize());*/
-
         }
     }
 }
